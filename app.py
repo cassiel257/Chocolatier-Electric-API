c3003db0f214e37bbb059a5d31f4017897baa78f
Tiffany Udoh <tiffany.udoh@gmail.com>
2021-03-23 04:22:09 -0400

    modified database schema

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import setup_db, Chocolate, Chocolatier
 
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
+  setup_db(app)
   CORS(app)
 
   @app.route('/')
@@ -15,17 +17,19 @@ def create_app(test_config=None):
 
   @app.route('/chocolates')
   def get_chocolates():
-    recipe=Chocolate.query.all()
-    return recipe
+    recipe=[c.format() for c in Chocolate.query.all()]
+    return jsonify({"Success":True, "Chocolates":recipe})
+
 
   @app.route('/chocolatiers')
   def get_chocolatiers():
-    recipe=Chocolatier.query.all()
-    return recipe
+    recipe=[c.format() for c in Chocolatier.query.all()]
+    return jsonify({"Success":True, "Chocolatiers":recipe})
+
 
   return app
 
-APP = create_app()
+app = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
